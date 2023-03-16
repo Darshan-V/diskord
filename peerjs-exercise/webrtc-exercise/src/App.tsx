24cc2b209d211973dbd8c7c23f0bbefd5a5471e4
Darshan-V <mr.darshanv@gmail.com>
2023-03-16 18:36:44 +0530

    trying peerjs

---
+++ peerjs-exercise/webrtc-exercise/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './components/Home'
+
+type Props = {}
+
+const App:React.FC<Props> = (_:Props) => {
+  return (
+    <div><Home/></div>
+  )
+}
+
+export default App
