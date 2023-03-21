c043bc52a65d807d187a3e2a256973dfe7e289d8
Darshan-V <mr.darshanv@gmail.com>
2023-03-21 16:20:18 +0530

    setuop global store -> redux

---
+++ frontend/src/store/store.ts
@@ -0,0 +1,9 @@
+import { configureStore, Store } from "@reduxjs/toolkit"
+import diskordReduser from "./features/diskord/diskordSlice"
+
+export type useAppDispatch = typeof store.dispatch
+export type useAppSelector = typeof store.getState
+
+export const store: Store<any> = configureStore({
+  reducer: diskordReduser
+})
