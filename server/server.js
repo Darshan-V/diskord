0b8e679648a7d3a668263b28f2d0bc9da2f451be
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-03-27 10:09:31 +0530

    rename dir backend to server

--- server/server.js
+++ server/server.js
@@ -8,14 +8,23 @@ import config from "./config.js"
 const PORT = config.port
 
 const app = express()
-app.use(cors({ origin: config.ORIGIN }))
+// app.use(cors({ origin: ["http://localhost:3001"] }))
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001")
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+//   next()
+// })
+
 app.use(express.json())
 
 const httpServer = http.createServer(app)
 
 const peerServer = ExpressPeerServer(httpServer)
 
-const io = new Server(httpServer, { cors: { origin: config.ORIGIN } })
+const io = new Server(httpServer, {
+  cors: { origin: ["http://localhost:3000"] }
+})
 
 io.on("connection", (socket) => {
   console.log(`Socket connected: ${socket.id}`)
@@ -26,6 +35,14 @@ io.on("connection", (socket) => {
 })
 
 app.use("/api/peer", peerServer) // mounting the peerjs server on the path /api/peer
+
+peerServer.on("connection", (client) => {
+  console.log("PeerClient connected", client.getId())
+})
+
+peerServer.on("disconnect", (client) => {
+  console.log("PeerClient disconnected")
+})
 
 app.get("/api", (req, res) => {
   res.send("Hello, World!")
--- test-front-end/src/App.js
+++ test-front-end/src/App.js
@@ -1,8 +1,41 @@
-import React, { useEffect } from "react"
-import peer from "peer"
+import React, { useEffect, useRef } from "react"
+import { Peer } from "peerjs"
+import { io } from "socket.io-client"
 
 function App() {
-  return <div>welcome</div>
+  const peerRef = useRef(null)
+  const selfPeerIdRef = useRef(null)
+
+  useEffect(() => {
+    const peer = new Peer(undefined, {
+      host: "localhost",
+      port: 3000,
+      path: "/api/peer"
+    })
+
+    peerRef.current = peer
+
+    peer.on("open", (id) => {
+      console.log("peerId", id)
+      selfPeerIdRef.current = id
+    })
+
+    return () => {
+      peer.disconnect()
+    }
+  }, [])
+
+  useEffect(() => {}, [])
+
+  function makeACall() {
+    var conn = peerRef.current.connect("dest-peer-id")
+  }
+
+  return (
+    <div>
+      <button onClick={makeACall}>Call</button>
+    </div>
+  )
 }
 
 export default App
