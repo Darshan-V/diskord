8c5b5491ef4192129bcf1c9fa37cb472f37f3fed
Darshan-V <mr.darshanv@gmail.com>
2023-03-27 14:54:26 +0530

    create nev create new server component

--- frontend/src/components/ServerSideBar/ServerSidebar.tsx
+++ frontend/src/components/ServerSideBar/ServerSidebar.tsx
@@ -3,9 +3,9 @@ import ServerIcons from "./ServerIcons"
 
 const ServerSidebar = () => {
   return (
-    <nav className="flex flex-col h-full w-20 bg-[#1E1F22]">
+    <div className="flex flex-col h-full w-20 bg-[#1E1F22]">
       <ServerIcons />
-    </nav>
+    </div>
   )
 }
 
