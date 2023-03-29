58c8f218c1b3c2030e454ff55b358eb26dfff1cf
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-03-29 10:13:37 +0530

    update workspacelist

--- Backend/controllers/workspaces.js
+++ Backend/controllers/workspaces.js
@@ -1,6 +1,6 @@
 export async function getWorkSpaceList(req, res) {
   const workSpaceList = {
-    workspaces: [{ 1: "wsp1" }, { 2: "wsp2" }, { 2: "wsp2" }]
+    workspaces: [{ 1: "wsp1" }, { 2: "wsp2" }, { 3: "wsp3" }]
   }
 
   res.json(workSpaceList)
