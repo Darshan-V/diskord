ad489d9fe35be740d05951e2c1516dd084a9fb03
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-03-30 13:34:12 +0530

    add validations and error handling

--- Backend/controllers/workspaces.js
+++ Backend/controllers/workspaces.js
@@ -1,14 +1,31 @@
 import { getWorkSpaces, getWorkspaceChannels } from "../models/workspaces.js"
 
 export async function getWorkSpaceList(req, res) {
-  const workSpaceList = await getWorkSpaces()
-
-  res.json(workSpaceList)
+  try {
+    const workSpaceList = await getWorkSpaces()
+    res.json(workSpaceList)
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
 }
 
 export async function getChannels(req, res) {
-  const wspId = req.params.id
-  const channels = await getWorkspaceChannels(wspId)
+  try {
+    const wspId = req.params.id
+    if (!Number.isInteger(Number(wspId))) {
+      return res.status(400).json({ err: "workspace id should be an integer" })
+    }
 
-  res.json(channels)
+    const channels = await getWorkspaceChannels(wspId)
+
+    // if(channels.length === 0) {
+    //   return res.status(400).json({err: ''})
+    // }
+
+    res.json(channels)
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
 }
