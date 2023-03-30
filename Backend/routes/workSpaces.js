d013dbc060f4a07c22fb66e463f597266f10001c
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-03-30 13:11:09 +0530

    getchannels from a workspace

--- Backend/routes/workSpaces.js
+++ Backend/routes/workSpaces.js
@@ -1,7 +1,8 @@
-import { getWorkSpaceList } from "../controllers/workspaces.js"
+import { getWorkSpaceList, getChannels } from "../controllers/workspaces.js"
 import express from "express"
 import "../models/workspaces.js"
 
 export const router = express.Router()
 
 router.get("/", getWorkSpaceList)
+router.get("/:id", getChannels)
