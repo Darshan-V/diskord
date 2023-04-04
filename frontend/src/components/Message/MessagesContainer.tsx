c9c7e8dba350bd115a5c15494d82bfd031de4788
Darshan-V <mr.darshanv@gmail.com>
2023-04-04 12:33:24 +0530

    dispatch serverid to the store in the message container

--- frontend/src/components/Message/MessagesContainer.tsx
+++ frontend/src/components/Message/MessagesContainer.tsx
@@ -1,15 +1,46 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
 import MessageInput from "./MessageInput"
 import MessageBoxHeader from "./MessageBoxHeader"
 import MessageBox from "./MessageBox"
+import {
+  setSelectedChannel,
+  setSelectedServer
+} from "../../store/features/diskord/diskordSlice"
 
 const MessagesContainer = () => {
+  interface dState {
+    activeChannel: string
+    activeServer: string
+  }
+
+  const dispatch = useDispatch()
+
+  const [messages, setMessages] = useState<[]>([])
+  const appState = useSelector((state: dState) => state)
+  const channelId = appState.activeChannel
+  const serverId = appState.activeServer
+  const params = useParams()
+
+  useEffect(() => {
+    if (params) {
+      dispatch(setSelectedServer(params.serverId))
+    }
+  })
+
   return (
-    <div className="flex flex-col w-full h-screen  bg-[#252526]">
-      <MessageBoxHeader />
-      <MessageBox />
-      <MessageInput />
-    </div>
+    <>
+      {channelId &&
+      serverId &&
+      params.channelId === String(channelId) ? (
+        <div className="flex flex-col w-full h-screen  bg-[#252526]">
+          <MessageBoxHeader />
+          <MessageBox messages={messages} />
+          <MessageInput setMessages={setMessages} />
+        </div>
+      ) : null}
+    </>
   )
 }
 
