7367633dfdca84abea74e5a07cf04c7657c14db1
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-04-06 11:08:36 +0530

    add oauth creds

--- Backend/config.js
+++ Backend/config.js
@@ -8,5 +8,11 @@ export default {
   port: envType ? process.env.PORT_Local : process.env.PORT_Prod,
   ORIGIN: process.env.CORS_ORIGIN,
   DB_PORT: process.env.DB_PORT,
-  DB_CONN_STRING: process.env.DB_CONN_STRING
+  DB_CONN_STRING: process.env.DB_CONN_STRING,
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  REDIRECT_URI: process.env.REDIRECT_URI,
+  AUTH_ENDPOINT: process.env.AUTH_ENDPOINT,
+  TOKEN_ENDPOINT: process.env.TOKEN_ENDPOINT,
+  USERINFO_ENDPOINT: process.env.USERINFO_ENDPOINT
 }
