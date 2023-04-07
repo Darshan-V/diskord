ce29a64bb0bd72f3a6ef4c006d0a7629ec497073
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-04-07 11:48:01 +0530

    handle error fk-constraint voilation

--- Backend/models/users.js
+++ Backend/models/users.js
@@ -11,16 +11,22 @@ const pool = new Pool({
 })
 
 export async function addUser(name, email) {
-  const res = await pool.query(
-    `insert into diskord.users(name,  email) 
-     values ($1, $2);`,
-    [name, email]
-  )
+  try {
+    const res = await pool.query(
+      `insert into diskord.users(gmail_name,  email) 
+         values ($1, $2);`,
+      [name, email]
+    )
+  } catch (err) {
+    if (err.code === "23505" && err.table === "users" && err.constraint === "users_email_key") {
+      throw new Error("email already registered")
+    }
+  }
 }
 
-export async function test() {
-  const res = await pool.query(`select * from diskord.users`)
-  console.log(res.rows)
-}
+// export async function test() {
+//   const res = await pool.query(`select * from diskord.users`)
+//   console.log(res.rows)
+// }
 
-// test()
+// // test()
