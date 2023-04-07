a88b1da86164bb8cb612e1d00c49f6fd79dfcaa4
Darshan-V <mr.darshanv@gmail.com>
2023-04-07 13:32:05 +0530

    add initial states of is loggedin

--- frontend/src/store/features/diskord/diskordSlice.ts
+++ frontend/src/store/features/diskord/diskordSlice.ts
@@ -4,14 +4,15 @@ import { io, Socket } from "socket.io-client"
 export interface DiskordState {
   activeServer: string
   activeChannel: string
-  socket : Socket
+  socket: Socket
+  isLoggedIn: boolean
 }
 
 const initialState: DiskordState = {
   activeServer: "",
   activeChannel: "",
-  socket:io('http://localhost:3000')
- 
+  socket: io("http://localhost:3000"),
+  isLoggedIn: false
 }
 
 const diskordSlice = createSlice({
@@ -24,11 +25,13 @@ const diskordSlice = createSlice({
     setSelectedChannel: (state, action) => {
       state.activeChannel = action.payload
     },
-   
+    setLoggedInStatus: (state, action) => {
+      state.isLoggedIn = action.payload
+    }
   }
 })
 
-export const { setSelectedServer, setSelectedChannel } =
+export const { setSelectedServer, setSelectedChannel, setLoggedInStatus } =
   diskordSlice.actions
 
 export default diskordSlice.reducer
