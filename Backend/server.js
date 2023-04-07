f8500836f37b4bfa6c278eab1de13159be268d9a
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-04-07 11:08:32 +0530

    remove oath from server

--- Backend/server.js
+++ Backend/server.js
@@ -5,62 +5,19 @@ import config from "./config.js"
 import sockets from "./sockets/sockets.js"
 import { router as workSpaceRouter } from "./routes/workSpaces.js"
 import { router as channelRouter } from "./routes/channels.js"
-import querystring from "querystring"
-import fetch from "node-fetch"
+import { router as sessionsRouter } from "./routes/sessions.js"
+import { router as usersRouter } from "./routes/users.js"
 
 const app = express()
 const port = config.port
 
-app.use(cors({ origin: "*", credentials: true }))
-
+app.use(cors({ origin: JSON.parse(config.ORIGIN), credentials: true }))
 app.use(express.json())
 
 app.use("/api/workspaces", workSpaceRouter)
 app.use("/api/workspaces/:id/channels", channelRouter)
-
-app.get("/api/session", (req, res) => {
-  const queryParams = querystring.stringify({
-    response_type: "code",
-    client_id: config.CLIENT_ID,
-    redirect_uri: config.REDIRECT_URI,
-    scope: "openid profile email"
-  })
-
-  return res.json({ url: `${config.AUTH_ENDPOINT}?${queryParams}` })
-})
-
-app.get("/api/sessions/oauth/google", async (req, res) => {
-  const { code } = req.query
-
-  const tokenParams = querystring.stringify({
-    code,
-    client_id: config.CLIENT_ID,
-    client_secret: config.CLIENT_SECRET,
-    redirect_uri: config.REDIRECT_URI,
-    grant_type: "authorization_code"
-  })
-
-  const tokenRes = await fetch(config.TOKEN_ENDPOINT, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body: tokenParams
-  })
-
-  const { access_token } = await tokenRes.json()
-
-  const userinfoOptions = {
-    headers: {
-      Authorization: `Bearer ${access_token}`
-    }
-  }
-
-  const userinfoRes = await fetch(config.USERINFO_ENDPOINT, userinfoOptions)
-  const { sub, name, email } = await userinfoRes.json()
-
-  res.send(`Welcome, ${name} (${email})!`)
-})
+app.use("/api/users", usersRouter)
+app.use("/api/sessions", sessionsRouter)
 
 const httpServer = createServer(app)
 sockets.init(httpServer)
