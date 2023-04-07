91f9194e7c909d33682e288773d7bc90864ac483
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-04-07 11:47:33 +0530

    handle re-registration

--- Backend/controllers/users/users.js
+++ Backend/controllers/users/users.js
@@ -16,41 +16,48 @@ export async function registerUserThroughGoogle(req, res) {
 }
 
 export async function registrationCallBack(req, res) {
-  const { code } = req.query
+  try {
+    const { code } = req.query
 
-  const tokenParams = querystring.stringify({
-    code,
-    client_id: config.CLIENT_ID,
-    client_secret: config.CLIENT_SECRET,
-    redirect_uri: config.REGISTRATION_CALLBACK,
-    grant_type: "authorization_code"
-  })
+    const tokenParams = querystring.stringify({
+      code,
+      client_id: config.CLIENT_ID,
+      client_secret: config.CLIENT_SECRET,
+      redirect_uri: config.REGISTRATION_CALLBACK,
+      grant_type: "authorization_code"
+    })
 
-  const tokenRes = await fetch(config.TOKEN_ENDPOINT, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body: tokenParams
-  })
+    const tokenRes = await fetch(config.TOKEN_ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: tokenParams
+    })
 
-  const { access_token } = await tokenRes.json()
+    const { access_token } = await tokenRes.json()
 
-  const userinfoRes = await fetch(config.USERINFO_ENDPOINT, {
-    headers: {
-      Authorization: `Bearer ${access_token}`
+    const userinfoRes = await fetch(config.USERINFO_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${access_token}`
+      }
+    })
+
+    const { Id, name, email } = await userinfoRes.json()
+
+    await addUser(name, email)
+
+    const sessionId = uuid()
+
+    //   await createSession(sessionId, email)
+    res
+      .cookie("sessionId", sessionId, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true })
+      .status(201)
+      .redirect(config.CLIENT_URL)
+  } catch (err) {
+    if (err.message === "email already registered") {
+      return res.status(400).json("email already registered, please login")
     }
-  })
-
-  const { Id, name, email } = await userinfoRes.json()
-
-  await addUser(name, email)
-
-  const sessionId = uuid()
-
-  //   await createSession(sessionId, email)
-  res
-    .cookie("sessionId", sessionId, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true })
-    .status(201)
-    .redirect(config.CLIENT_URL)
+    return res.status(500).json()
+  }
 }
