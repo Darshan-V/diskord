b5d26a49ea5e31f0e41cc352be41e8d126204784
Darshan-V <mr.darshanv@gmail.com>
2023-04-07 13:31:42 +0530

    include regestration page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,24 @@
 import React from "react"
+import { useSelector } from "react-redux"
 import { Routes, Route } from "react-router-dom"
 import LeftNavbar from "./components/LeftNavbar/LeftNavbar"
 import MessagesContainer from "./components/Message/MessagesContainer"
+import Signup from "./components/Regestration/Signup"
 
 function App() {
+  interface dState {
+    isLoggedIn: boolean
+  }
+
+  const isLoggedIn = useSelector(
+    (state: dState) => state.isLoggedIn
+  )
+
   return (
     <div className="flex w-screen h-screen bg-[#28292d]">
-      <LeftNavbar />
+      <>{isLoggedIn ? <LeftNavbar /> : ""}</>
       <Routes>
+        <Route path="/" element={<Signup />} />
         <Route
           path="/diskord/servers/:serverId/channels?/:channelId?"
           element={<MessagesContainer />}
